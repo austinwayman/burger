28f882e5d420c08323e4d64cc8ab9c9809dbc134
Austin Wayman <awguitar@msn.com>
2018-11-14 14:14:59 -0700

    finish

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double Animal Style', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+
+SELECT * FROM burgers;
